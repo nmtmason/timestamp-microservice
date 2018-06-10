99171a7d5895a4b4e9e790c33b550dcc69acf1d6
Nicholas Mason <nmt.mason@googlemail.com>
2018-06-10 10:00:26 +0100

    Run index.js through prettier

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ var url = require('url');
 
 var handler = {
   routes: [],
-  addRoute: function (pattern, handler) {
+  addRoute: function(pattern, handler) {
     this.routes.push({ pattern: pattern, handler: handler.bind(this) });
   },
-  getRoute: function getRoute (pathname) {
+  getRoute: function getRoute(pathname) {
     for (var i = 0; i < this.routes.length; i++) {
       var route = this.routes[i];
       if (route.pattern.test(pathname)) {
@@ -19,7 +19,7 @@ var handler = {
 
 var routes = Object.create(handler);
 
-routes.createResponse = function (date) {
+routes.createResponse = function(date) {
   var months = [
     'January',
     'February',
@@ -37,23 +37,28 @@ routes.createResponse = function (date) {
 
   return JSON.stringify({
     unix: Math.floor(date.getTime() / 1000),
-    natural: months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
+    natural:
+      months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear()
   });
 };
-routes.addRoute(/^\/(\d+)$/, function (request, response, matches) {
+routes.addRoute(/^\/(\d+)$/, function(request, response, matches) {
   response.write(this.createResponse(new Date(Number(matches[0]) * 1000)));
   response.end();
 });
-routes.addRoute(/^\/(\w+) (\d{1,2}), (\d{4})$/, function (request, response, matches) {
+routes.addRoute(/^\/(\w+) (\d{1,2}), (\d{4})$/, function(
+  request,
+  response,
+  matches
+) {
   response.write(this.createResponse(new Date(matches[0])));
   response.end();
 });
-routes.addRoute(/^\/.*$/, function (request, response) {
+routes.addRoute(/^\/.*$/, function(request, response) {
   response.write(JSON.stringify({ unix: null, natural: null }));
   response.end();
 });
 
-var server = http.createServer(function (request, response) {
+var server = http.createServer(function(request, response) {
   var pathname = url.parse(request.url).pathname;
   var sanitized = pathname.replace(/%20/g, ' ');
   var route = routes.getRoute(sanitized);
